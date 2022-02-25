997cccbb88b5504de3ccd53e024c2585514dc802
Toge5 <touko.o.sillanpaa@gmail.com>
2022-02-25 12:52:17 +0200

    L02-T1

---
+++ main.py
@@ -0,0 +1,6 @@
+nimi = input('Anna nimesi: ')
+kok = int(input('Anna kokonaisluku: '))
+des = float(input('Anna desimaaliluku: '))
+print(f'{nimi} desimaaliluku {des} potenssiin {kok} on {des**kok}')
+
+
